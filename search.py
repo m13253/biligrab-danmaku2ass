5ec045739e1e6450c65c2296f6cc28cbad518302
910JQK <v910JQK@gmail.com>
2014-06-15 15:18:21 +0800

    Updated search tool

--- search.py
+++ search.py
@@ -1,36 +1,29 @@
 #!/usr/bin/python3
 
 import sys
-import urllib.request
 import urllib.parse
-import gzip
-from bs4 import BeautifulSoup
+import json
+from biligrab import urlfetch
 
 def print_hl(str):
     print ('\033[1m %s \033[0m' % str)
 
 
 USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36'
-URL = 'http://www.bilibili.tv/search?keyword=' + urllib.parse.quote(sys.argv[1]);
+APPKEY = '876fe0ebd0e67a0f'
 
 
+def main():
+    _, resp = urlfetch('http://api.bilibili.tv/search?type=json&appkey=' + APPKEY + '&keyword=' + urllib.parse.quote(sys.argv[1]))
+    data = json.loads( resp.decode('utf-8', 'replace') )
+    for k, v in data['result'].items():
+        print_hl(' >> ' + v['title']);
+        if v['type'] == 'video':
+            print('  [Video] Author:' + v['author'] + ' Play:' + v['play'] + ' Favorites:' +  v['favorites'] + ' Review:' + v['review'] + ' Comment:' + v['video_review']);
+            print('  http://www.bilibili.tv/video/av' + str(v['aid']) + "\n")
+        elif v['type'] == 'special':
+            print("  [Special]")
+            print('  http://www.bilibili.tv/sp/' + str(v['spid']) + "\n")
 
-req_headers = {'User-Agent': USER_AGENT, 'Accept-Encoding': 'gzip, deflate'}
-req = urllib.request.Request(url=URL, headers=req_headers)
-response = urllib.request.urlopen(req, timeout=120)
-data = gzip.GzipFile(fileobj=response).read()
-
-soup = BeautifulSoup(data)
-target = soup.findAll(attrs={'class':'l'})
-
-for i in target:
-    _url = i.find('a').attrs['href']
-    _t = i.find(attrs={'class':'t'})
-    _ca = _t.find('span').contents[0]
-    _t.find('span').clear()
-    _tl = ''
-    for string in _t.strings:
-        _tl += string
-    print_hl('>> ' + _tl)
-    print(_ca + ' ' + _url + "\n")
-
+if __name__ == '__main__':
+    sys.exit(main())
